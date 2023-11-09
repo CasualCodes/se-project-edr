03d1dbd47367d444e9d7051ec01b0dcc34acaaf7
CasualCodes <95406523+CasualCodes@users.noreply.github.com>
2023-11-09 14:17:35 +0800

    Update ui_screens.py

--- ui_screens.py
+++ ui_screens.py
@@ -57,38 +57,3 @@ result_screen = """
 """
 
 screens = screen_manager + main_screen + assessment_screen + result_screen
-
-"""
-
-<MainScreen>:
-    name: 'menu'
-    MDRectangleFlatButton:
-        text: 'Profile'
-        pos_hint: {'center_x':0.5,'center_y':0.6}
-        on_press: root.manager.current = 'profile'
-    MDRectangleFlatButton:
-        text: 'Upload'
-        pos_hint: {'center_x':0.5,'center_y':0.5}
-        on_press: root.manager.current = 'upload'
-    
-<ProfileScreen>:
-    name: 'profile'
-    MDLabel:
-        text: 'Profile'
-        halign: 'center'
-    MDRectangleFlatButton:
-        text: 'Back'
-        pos_hint: {'center_x':0.5,'center_y':0.1}
-        on_press: root.manager.current = 'menu'
-        
-<UploadScreen>:
-    name: 'upload'
-    MDLabel:
-        text: 'Upload'
-        halign: 'center'
-    MDRectangleFlatButton:
-        text: 'Back'
-        pos_hint: {'center_x':0.5,'center_y':0.1}
-        on_press: root.manager.current = 'menu'
-
-"""
