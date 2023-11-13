fe90d94eed87f4d77d59e8f24fd46ecfb7cb6f9e
CasualCodes <95406523+CasualCodes@users.noreply.github.com>
2023-11-13 14:12:30 +0800

    added label edit in main again (defeated the nemesis)

--- front_end.py
+++ front_end.py
@@ -49,7 +49,7 @@ screen_manager.add_widget(ResultsScreen(name='result'))
 # Main Front End Execution
 class front_end_main(MDApp):
     def build(self):
-        screen = Builder.load_string(ui_screens.screens)
+        self.screen = Builder.load_string(ui_screens.screens)
         self.theme_cls.theme_style = "Light"
         self.theme_cls.primary_palette = "Blue"
         self.manager_open = False
@@ -58,8 +58,8 @@ class front_end_main(MDApp):
             select_path=self.select_path,
             preview=True,
         )
-    
-        return screen
+
+        return self.screen
 
     def file_manager_open(self):
         self.file_manager.show('/')  # output manager to the screen
@@ -79,7 +79,7 @@ class front_end_main(MDApp):
         global outputData
         # processinput takes the PATH of the image, and then updates outputData of the results
         outputData = back_end.processInput(path)
-
+        self.screen.get_screen(name='result').ids.lb.text = outputData
         global inputData
         inputData = path
         return path
@@ -88,3 +88,29 @@ class front_end_main(MDApp):
         '''Called when the user reaches the root of the directory tree.'''
 
         self.file_manager.close()
+
+
+
+# class MainScreen(Screen):
+#     pass
+
+# class AssessmentScreen(Screen):
+#     def callBackEnd(self):
+#         global inputData
+#         print("InputData " + inputData)
+#         # inputData = GET INPUT FROM CAMERA/GALLERY
+
+#         #root.callBackEnd()
+
+#         global outputData
+#         # processinput takes the PATH of the image, and then updates outputData of the results
+#         # outputData = back_end.processInput(inputData)
+
+# class ResultsScreen(Screen):
+#     definition = StringProperty()
+#     # update is what displays the results in result page
+#     def update(self):
+#         global outputData
+#         self.ids.lb.text = outputData
+#     def clear(self):
+#         self.ids.lb.text = ""
