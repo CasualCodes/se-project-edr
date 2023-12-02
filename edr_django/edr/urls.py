76b19014c7cac5a2c9869f23dfa6d6891c70839f
jal9o3 <119987573+jal9o3@users.noreply.github.com>
2023-12-02 19:38:28 +0800

    removed legacy code in urls py

--- edr_django/edr/urls.py
+++ edr_django/edr/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path("assess/", views.assess, name="assess"),
     path("list/", views.list, name="list"),
     path("results/", views.results, name="list"),
-    # path('my_view/', views.my_view, name='my-view'),
 ]
