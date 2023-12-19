32866ea0226f5f6651a2fb4e2cab6ded740ea62b
jal9o3 <119987573+jal9o3@users.noreply.github.com>
2023-12-19 18:59:57 +0800

    now loading model when opening homepage

--- edr_django/edr/views.py
+++ edr_django/edr/views.py
@@ -21,6 +21,26 @@ loaded_model = tensorflow.keras.models.load_model('edr/model.h5')
 class_labels = list(["Cataract", "Conjunctivitis", "Ectropion", "Normal", "Pterygium", "Trachoma"])
 
 def index(request):
+    #ATTEMPT AT PRELOADING
+    print("Preloading model or something...")
+    
+    ## DATA PREPROCESS ##
+    test_image_path = 'media/sharingan'
+    img = image.load_img(test_image_path, target_size=(256, 256))
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array /= 255.  # Normalize the image
+
+    ## INPUT TO MODEL ##
+    prediction = loaded_model.predict(img_array)
+
+    ## RESULT ##
+    # Get Result
+    predicted_class = np.argmax(prediction, axis=-1)
+    predicted_label = class_labels[predicted_class[0]] # The Output
+    print("Sharingan!")
+    print("Preloading Result: "+predicted_label)
+    
     context = {}
     return render(request, "edr/index.html", context)
 
