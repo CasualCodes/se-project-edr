a9765f74d83476cedf135a49df89eeb6b0e962e2
SyJanWilhelm <janwilhelmtan.sy@bicol-u.edu.ph>
2024-02-21 13:17:12 +0800

    UI Mockup
    Important UI (Assess only)

---
+++ Project - AIDentifye/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('.menu-icon').addEventListener('click', function() {
+        document.querySelector('nav ul').classList.toggle('active');
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('.insideMenu').addEventListener('click', function() {
+        document.querySelector('nav ul').classList.toggle('active');
+    });
+});
+
